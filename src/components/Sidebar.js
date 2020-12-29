8068c0457c67de3a64514f8e8e2188f8f950711a
hulya <hulyakarakayaa06@gmail.com>
2020-12-28 23:46:23 -0800

    create the sidebar and blog template

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,8 +2,22 @@ import React from "react"
 import Links from "../constants/links"
 import SocialLinks from "../constants/socialLinks"
 import { FaTimes } from "react-icons/fa"
+
 const Sidebar = () => {
-  return <h2>sidebar component</h2>
+  const isOpen = false;
+  return (
+    <aside className={`sidebar ${isOpen ? "show-sidebar" :  "" }`}>
+      {/* toggle sidebar */}
+      <button className="close-btn">
+        <FaTimes />
+      </button>
+      <div className="side-container">
+        <Links styleClass="sidebar-links" />
+        <SocialLinks styleClass="sidebar-icons" />
+      </div>
+    </aside>
+  )
 }
 
 export default Sidebar
+  
