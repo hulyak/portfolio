f26000e27c029d69a55a2b7cddc527f8df461af0
hulya <hulyakarakayaa06@gmail.com>
2021-01-24 19:08:20 -0800

    update styles and add analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,11 +7,26 @@
 module.exports = {
   siteMetadata: {
     title: "Hulya Karakaya Portfolio",
-    description: "This is Portfolio Site for Hulya Karakaya",
+    description: "This is the Portfolio Site for Hulya Karakaya",
     author: "hulya karakaya",
-    twitterUsername: "@hulyakarakayaa",
+    twitterUsername: "@hulyakarkya",
     image: "/twitter-img.png",
-    siteUrl: "https://hulyak.netlify.app/",
+    siteUrl: "https://hulya.tech/",
+    keywords: [
+      "hulya",
+      "karakaya",
+      "tutorial",
+      "code",
+      "javascript",
+      "css",
+      "web",
+      "html",
+      "courses",
+      "react",
+      "gatsby",
+      "productivity",
+      "life",
+    ],
   },
   flags: {
     THE_FLAG: false,
@@ -19,13 +34,46 @@ module.exports = {
   plugins: [
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-transformer-json`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sitemap`,
+    `gatsby-plugin-svgr`,
+
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "G-64464WR2DB",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-hotjar`,
+      options: {
+        id: 1152714,
+        sv: 6,
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `assets`,
         path: `${__dirname}/src/assets/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/src/templates`,
       },
     },
     {
@@ -73,5 +121,49 @@ module.exports = {
     //     ],
     //   },
     // },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-embed-video`,
+            options: {
+              width: 800,
+              related: false, //Optional: Will remove related videos from the end of an embedded YouTube video.
+              noIframeBorder: true, //Optional: Disable insertion of <style> border: 0
+            },
+          },
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 800,
+              tracedSVG: true,
+              showCaptions: true,
+            },
+          },
+          {
+            resolve: "@weknow/gatsby-remark-twitter",
+            options: {
+              align: "center",
+            },
+          },
+          {
+            resolve: "gatsby-remark-prismjs",
+            options: {
+              inlineCodeMarker: "÷",
+              showLineNumbers: false,
+            },
+          },
+          `gatsby-remark-static-images`,
+          {
+            resolve: "@weknow/gatsby-remark-codepen",
+            options: {
+              theme: "dark",
+              height: 400,
+            },
+          },
+        ],
+      },
+    },
   ],
 }
